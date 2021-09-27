63289dc845be6fb37c317f35781dd0205f4bca4d
MD. Shahriar Rahman <srshawon11@gmail.com>
2021-09-28 03:53:12 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_tutorial/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+
+      //initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
