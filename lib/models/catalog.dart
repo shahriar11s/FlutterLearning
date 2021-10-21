4da80dc1e32f7bc806a46b844a5d1f88016bf0c9
MD. Shahriar Rahman <srshawon11@gmail.com>
2021-10-22 05:20:38 +0600

    cart works

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,7 +1,16 @@
+// ignore_for_file: null_closures
+
 import 'dart:convert';
 
 class CatalogModel {
   static List<Item> items = [];
+
+// Get Item by ID
+  Item getById(int id) =>
+      items.firstWhere((element) => element.id == id, orElse: null);
+
+  // Get Item by position
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
